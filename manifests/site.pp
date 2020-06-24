bfcef7150b621abd537975cfda29fa34c075b237
jasinger <jasinger@fs.fed.us>
2020-06-24 17:08:02 -0500

    Update site.pp

--- manifests/site.pp
+++ manifests/site.pp
@@ -1,18 +1,3 @@
 node default {
-  file { '/root/README':
-    ensure => file,
-    content => 'This is a readme',
-    owner   => 'root',
-  }
-}
-node 'master.puppet.vm' {
-  include role::master_server
-}
 
-node /^web/ {
-  include role::app_server
-}
-
-node /^db/ {
-  include role::app_server
 }
